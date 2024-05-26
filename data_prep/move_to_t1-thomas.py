f9df6d63e1a46e051e58f5e134a3da27e17dcfb3
zanberry <zanberry17@gmail.com>
2024-05-26 11:58:20 +0100

    added tempr move to script

--- data_prep/move_to_t1-thomas.py
+++ data_prep/move_to_t1-thomas.py
@@ -2,7 +2,7 @@
 import os
 
 # Change the name of last folder in the line below to the desired dir
-ROOT_PATH = "/storage/research/cinn_comp/ThalSR/zan/derivatives/singularity_prep_seg/fmriprep/495-556"
+ROOT_PATH = "/storage/research/cinn_comp/ThalSR/zan/derivatives/singularity_prep_seg/fmriprep/0-98"
 
 # Move target folders inside a newly created, specified folder
 for item in os.listdir(ROOT_PATH):
@@ -25,10 +25,12 @@ for item in os.listdir(ROOT_PATH):
     from_left_path = current_path+"/left"
     from_right_path = current_path+"/right"
     from_temp_path = current_path+"/temp"
+    from_tempr_path = current_path+"/tempr"
     # Define new locations for the target folders
     to_left_path = t1thomas_path+"/left"
     to_right_path = t1thomas_path+"/right"
     to_temp_path = t1thomas_path+"/temp"
+    to_tempr_path = t1thomas_path+"/tempr"
 
     # Move target folders
     if os.path.exists(from_left_path):
@@ -37,5 +39,7 @@ for item in os.listdir(ROOT_PATH):
         os.rename(from_right_path, to_right_path)
     if os.path.exists(from_temp_path):
         os.rename(from_temp_path, to_temp_path)
+    if os.path.exists(from_tempr_path):
+        os.rename(from_tempr_path, to_tempr_path)
 
 
