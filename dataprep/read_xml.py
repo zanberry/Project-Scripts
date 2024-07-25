cbdabbc748bfc7abc2b7fef7e5f852773d47e7c0
zanberry <zanberry17@gmail.com>
2024-07-25 16:14:20 +0100

    changed this so it gets nuclei2 col

--- dataprep/read_xml.py
+++ dataprep/read_xml.py
@@ -6,24 +6,16 @@ from pathlib import Path
 import os
 
 ROOT = "/storage/research/cinn_comp/ThalSR/zan/derivatives/evalseg_output"
-NUCLEI_LIST = [2, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+NUCLEI_LIST_1 = [2, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+NUCLEI_LIST_2 = [2, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 SEG_LIST = ["t1thomas", "hipsthomas"]
 
 # Extracts relevant information from the .xml file
-def extract_info(filepath):
+def extract_avgdist(filepath):
     
     tree = ET.parse(filepath)
     # Get the parent tag of the xml document
     root = tree.getroot()
-    # Get the name of the moving image file (discards rest of the path)
-    filename = Path(root.find("moving-image").attrib["filename"]).name
-
-    # Remove file extensions
-    filename = filename.split(".")[0]
-    # Replace hyphens with underscores
-    filename = filename.replace("-", "_")
-    # Split filenames by underscores and turn them into a list
-    file_info = filename.split("_")
 
     # Check if missing AVGDIST value
     r_avgdist = "NA"
@@ -33,38 +25,45 @@ def extract_info(filepath):
     else:
         print(f"No avgdist in: {filepath}")
 
-    # Return all relvant values, skipping "sub-"
-    return [
-        file_info[1],
-        file_info[2],
-        file_info[3],
-        file_info[4],
-        r_avgdist   
-    ]
+    return r_avgdist
 
 infos = []
 
+missingsubs = []
+
 for sub in range(0,557):
     for seg in SEG_LIST:
-        for nuclei in NUCLEI_LIST:
-            
-            # Define paths
-            path_to_left_xml = f"{ROOT}/sub-{sub}/{seg}/left/sub-{sub}_evalseg_left_KM-{nuclei}_T1-{nuclei}.xml"
-            path_to_right_xml = f"{ROOT}/sub-{sub}/{seg}/right/sub-{sub}_evalseg_right_KM-{nuclei}_T1-{nuclei}.xml"
 
-            if Path(path_to_left_xml).exists():
-                infos.append(extract_info(path_to_left_xml))
+        if not Path(f"{ROOT}/sub-{sub}/{seg}").exists():
+            continue
 
-            if Path(path_to_right_xml).exists():
-                infos.append(extract_info(path_to_right_xml))
+        for hemisphere in ["left", "right"]:
+            for nuclei1 in NUCLEI_LIST_1:
+                for nuclei2 in NUCLEI_LIST_2:
+                
+                    path_to = f"{ROOT}/sub-{sub}/{seg}/{hemisphere}/sub-{sub}_evalseg_{hemisphere}_KM-{nuclei1}_T1-{nuclei2}.xml"
+
+                    if not Path(path_to).exists(): 
+                        print(f"{path_to} does not exist, skipped!")
+                        missingsubs.append(sub)
+                        continue
+
+                    # print(f"Running {path_to}")
+
+                    avgdist = extract_avgdist(path_to)
+                    info = [ sub, seg, hemisphere, nuclei1, nuclei2, avgdist ]
+
+                    infos.append(info)
+
+print(set(missingsubs))
 
 # Create and write in csv
-with open('fthp_metrics.csv', 'w', newline='') as file:
+with open('metrics_test.csv', 'w', newline='') as file:
     
     # Define var to write inside csv
     writer = csv.writer(file)
     # Define var which is a list of col names
-    field = ["scanID","segmethod","hemisphere","nuclei","AVGDIST"]
+    field = ["scanID","segmethod","hemisphere","nuclei1", "nuclei2", "AVGDIST"]
 
     # Write col names
     writer.writerow(field)
